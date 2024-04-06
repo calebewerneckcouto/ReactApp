22d6bd71c5fbd486b07ce5d7a0f84bf747e23e15
Calebe <calebewerneck@hotmail.com>
2024-04-06 18:00:07 -0300

    Aparecendo dados no Carrinho

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,17 +1,46 @@
-import React from "react";
-import {CartArea,CartHeader,CartBody,CartIcon,CartText } from './styled';
+import React ,{useState} from "react";
 
+import {CartArea,CartHeader,CartBody,CartIcon,CartText,ProductsArea,ProductItem,ProductPhoto,ProductInfoArea,ProductName,ProductPrice,ProductQuantityArea } from './styled';
+import { useSelector } from "react-redux";
 
 export default () => {
+
+    const products = useSelector(state => state.cart.products);
+
+    const handleCartClick = () => {
+        setShow(!show);
+    }
+
+    const [show, setShow] = useState(true);
     return (
         <CartArea>
-            <CartHeader>
+            <CartHeader onClick={handleCartClick}>
                 <CartIcon src="/assets/cart.png" />
-                <CartText>Meu Carrinho (x)</CartText>
+                <CartText>Meu Carrinho ({products.length})</CartText>
+                {show &&
+                    <CartIcon src="/assets/down.png" />
+                }
             </CartHeader>
 
-            <CartBody>
+            <CartBody show={show}>
+                <ProductsArea>
+                    
+                    {products.map((item, index) => (
+                        
+                        <ProductItem key={index}>
+                        <ProductPhoto src={item.image} />
+                        <ProductInfoArea>
+                                <ProductName>{item.name}</ProductName>
+                             <ProductPrice>R$ {item.price.toFixed(2)}</ProductPrice>
+                        </ProductInfoArea>
+                        <ProductQuantityArea>
 
+                        </ProductQuantityArea>
+                    </ProductItem>
+                    
+                ))}
+                    
+               </ProductsArea>
             </CartBody>
         </CartArea>
     );
